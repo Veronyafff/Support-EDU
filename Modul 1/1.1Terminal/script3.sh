b6b4d39f5c9ad7c3ba23d2cb6b6fd7b824e2a2bc
Veronika Mordovina <mordovinav0@gmail.com>
2025-07-08 11:52:22 +0300

    Добавлен тестовый файл

---
+++ Modul 1/1.1Terminal/script3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "Введите ваше имя: " username
+mkdir "$username"
+cd /c/Users/vimordovina/my_first_project/$username
+echo "Привет, $username! Это твоя первая папка." > welcome.txt
