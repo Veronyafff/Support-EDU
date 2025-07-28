199d6a985a9fe0978693a73f25aedeb328b43f9e
Veronyafff <mordovinav0@gmail.com>
2025-07-28 23:19:09 +0300

    Delete script1.sh

--- script1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-read -p "Введите ваше имя: " username
-mkdir "$username"
-cd /c/Users/vimordovina/my_first_project/$username
-echo "Привет, $username! Это твоя первая папка." > welcome.txt
-
